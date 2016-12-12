bf8f46a8e4dbd78fb12ed60909531b34fdfccb36
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2016-12-12 15:57:38 -0500

    Fixing some more linting problems and refactored out unneeded Command.run calls

--- HgRepo.js
+++ HgRepo.js
@@ -1,6 +1,4 @@
-const Promise = require('bluebird');
 const Fs = require('fs-extra-promise');
-const Path = require('path');
 const Command = require('./utils/Command');
 
 module.exports = class HgRepo {
@@ -24,28 +22,43 @@ module.exports = class HgRepo {
   init(done = undefined) {
     return Command.run('init', this.path)
       .catch((error) => {
-        console.log(error)
+        console.log(error);
       })
       .asCallback(done);
   }
 
   commit(message, done = undefined) {
     Command.run('commit', this.path, ['-m', message])
-      .asCallback(done)
+      .asCallback(done);
   }
 
   add(options, done = undefined) {
     Command.run('add', this.path, options)
-      .asCallback(done)
+      .asCallback(done);
   }
 
   push(options, done = undefined) {
     Command.run('push', this.path, options)
-      .asCallback(done)
+      .asCallback(done);
   }
 
   pull(options, done = undefined) {
     Command.run('pull', this.path, options)
-      .asCallback(done)
+      .asCallback(done);
+  }
+
+  update(options, done = undefined) {
+    Command.run('update', this.path, options)
+      .asCallback(done);
+  }
+
+  rename(options, done = undefined) {
+    Command.run('rename', this.path, options)
+      .asCallback(done);
+  }
+
+  rename(options, done = undefined) {
+    Command.run('merge', this.path, options)
+      .asCallback(done);
   }
 };
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const HgRepo = require('./HgRepo');
-const Command = require('./utils/Command');
 const Promise = require('bluebird');
 const ShortID = require('shortid');
 const Fs = require('fs-extra-promise');
 const Path = require('path');
+
+const HgRepo = require('./HgRepo');
+const Command = require('./utils/Command');
 
 /*
 The public facing API for various common Mercurial tasks.
@@ -47,42 +48,42 @@ function mergeRepositories(fromRepo, combinedRepo) {
 
   authFrom = `https://${fromRepo.username}:${fromRepo.password}@${fromRepo.url.split('@').pop()}`;
 
-  return Command.run('pull', combinedRepo.path, ['-f', authFrom])
-    .then(() => Command.run('update', combinedRepo.path, ['-C', 'tip']))
+  return combinedRepo.pull(['-f', authFrom])
+    .then(() => combinedRepo.update(['-C', 'tip']))
     .then(() => Fs.ensureDirAsync(Path.resolve(combinedRepo.path, repoDirectory)))
     .catch((error) => {
       repoDirectory += uuid;
       Fs.ensureDirAsync(Path.resolve(combinedRepo.path, repoDirectory))
     })
-    .then(() => Command.run('rename', combinedRepo.path, ['*', repoDirectory]))
-    .then(() => Command.run('commit', combinedRepo.path, [`-m "Moving repository ${repoName} into folder ${repoName}"`]))
-    .then(() => Command.run('merge', combinedRepo.path))
-    .then(() => Command.run('commit', combinedRepo.path, [`-m "Merging ${repoName} into combined"`]))
-    .catch((error) => {
-      if (!error.message.includes("nothing to merge")) throw error;
-    })
-}
-
-function cloneOrMergeMany(from, to) {
-  const newRepo = new HgRepo(to)
-  let authFrom = null;
-
-  if (typeof from == 'object' && !Array.isArray(from)) {
-    authFrom = `https://${from.username}:${from.password}@${from.url.split('@').pop()}`;
-
-    return Command.run('clone', newRepo.path, [authFrom])
-      .then(() => newRepo)
-  } else if (Array.isArray(from)) {
-    return newRepo.init()
-      .then(() => {
-        return Promise.each(from, (fromRepo) => {
-          return mergeRepositories(fromRepo, newRepo);
-        })
+    .then(() => combinedRepo.rename(['*', repoDirectory]))
+    .then(() => combinedRepo.commit(`Moving repository ${repoName} into folder ${repoName}`))
+    .then(() => combinedRepo.merge)
+    .then(() => combinedRepo.commit(`Merging ${repoName} into combined`)
+      .catch((error) => {
+        if (!error.message.includes("nothing to merge")) throw error;
       })
-      .then(() => newRepo);
-  } else {
-    throw new TypeError("Incorrect type of from parameter. Must be an array or object")
-  }
-}
+    }
 
-module.exports = Hg;
+  function cloneOrMergeMany(from, to) {
+    const newRepo = new HgRepo(to)
+    let authFrom = null;
+
+    if (typeof from == 'object' && !Array.isArray(from)) {
+      authFrom = `https://${from.username}:${from.password}@${from.url.split('@').pop()}`;
+
+      return Command.run('clone', newRepo.path, [authFrom])
+        .then(() => newRepo)
+    } else if (Array.isArray(from)) {
+      return newRepo.init()
+        .then(() => {
+          return Promise.each(from, (fromRepo) => {
+            return mergeRepositories(fromRepo, newRepo);
+          })
+        })
+        .then(() => newRepo);
+    } else {
+      throw new TypeError("Incorrect type of from parameter. Must be an array or object")
+    }
+  }
+
+  module.exports = Hg;
