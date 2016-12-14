b81d3b2bf422b71b73f7761de831df348cd60098
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2016-12-14 13:03:53 -0500

    Fixing more linting errors

--- index.js
+++ index.js
@@ -6,17 +6,62 @@ const Path = require('path');
 const HgRepo = require('./HgRepo');
 const Command = require('./utils/Command');
 
+function mergeRepositories(fromRepo, combinedRepo) {
+  const uuid = `-${ShortID.generate()}`;
+  const repoName = fromRepo.url.split('/').pop();
+  let repoDirectory = repoName;
+  let authFrom;
+
+  if (fromRepo.url.includes('ssh')) throw new Error('SSHNotSupported');
+  if (fromRepo.url.includes('https')) {
+    authFrom = `https://${fromRepo.username}:${fromRepo.password}@${fromRepo.url.split('@').pop()}`;
+  } else {
+    authFrom = fromRepo.url;
+  }
+
+  return combinedRepo.pull(['-f', authFrom])
+    .then(() => combinedRepo.update(['-C', 'tip']))
+    .then(() => Fs.ensureDirAsync(Path.resolve(combinedRepo.path, repoDirectory)))
+    .catch(() => {
+      repoDirectory += uuid;
+      Fs.ensureDirAsync(Path.resolve(combinedRepo.path, repoDirectory));
+    })
+    .then(() => combinedRepo.rename(['*', repoDirectory]))
+    .then(() => combinedRepo.commit(`Moving repository ${repoName} into folder ${repoName}`))
+    .then(() => combinedRepo.merge)
+    .then(() => combinedRepo.commit(`Merging ${repoName} into combined`))
+    .catch((error) => {
+      if (!error.message.includes('nothing to merge')) throw error;
+    });
+}
+
+function cloneOrMergeMany(from, to) {
+  const newRepo = new HgRepo(to);
+  let authFrom = null;
+
+  if (typeof from === 'object' && !Array.isArray(from)) {
+    authFrom = `https://${from.username}:${from.password}@${from.url.split('@').pop()}`;
+
+    return Command.run('clone', newRepo.path, [authFrom])
+      .then(() => newRepo);
+  } else if (Array.isArray(from)) {
+    return newRepo.init()
+      .then(() => Promise.each(from, fromRepo => mergeRepositories(fromRepo, newRepo)))
+      .then(() => newRepo);
+  } else {
+    throw new TypeError('Incorrect type of from parameter. Must be an array or object');
+  }
+}
+
 /*
 The public facing API for various common Mercurial tasks.
 */
 const Hg = {
   /**
    * Cloning
-   * 
    * @param  {Array<Object>| Object}   from [description]
    * @param  {url:String, user:String, pass:String, path:String}   to   [description]
    * @param  {Function} done [description]
-   * 
    * @return {[type]}        [description]
    */
   clone(from, to = undefined, done = undefined) {
@@ -36,54 +81,7 @@ const Hg = {
     return Command.run('--version')
       .then(output => console.log(output))
       .asCallback(done);
-  }
-}
-
-function mergeRepositories(fromRepo, combinedRepo) {
-  const uuid = `-${ShortID.generate()}`;
-  const repoName = fromRepo.url.split('/').pop();
-  let repoDirectory = repoName
-
-  if (!fromRepo.url.includes('https')) throw new Error("OnlyHTTPSSupported");
-
-  authFrom = `https://${fromRepo.username}:${fromRepo.password}@${fromRepo.url.split('@').pop()}`;
-
-  return combinedRepo.pull(['-f', authFrom])
-    .then(() => combinedRepo.update(['-C', 'tip']))
-    .then(() => Fs.ensureDirAsync(Path.resolve(combinedRepo.path, repoDirectory)))
-    .catch((error) => {
-      repoDirectory += uuid;
-      Fs.ensureDirAsync(Path.resolve(combinedRepo.path, repoDirectory))
-    })
-    .then(() => combinedRepo.rename(['*', repoDirectory]))
-    .then(() => combinedRepo.commit(`Moving repository ${repoName} into folder ${repoName}`))
-    .then(() => combinedRepo.merge)
-    .then(() => combinedRepo.commit(`Merging ${repoName} into combined`))
-    .catch((error) => {
-      if (!error.message.includes("nothing to merge")) throw error;
-    })
-}
-
-function cloneOrMergeMany(from, to) {
-  const newRepo = new HgRepo(to)
-  let authFrom = null;
-
-  if (typeof from == 'object' && !Array.isArray(from)) {
-    authFrom = `https://${from.username}:${from.password}@${from.url.split('@').pop()}`;
-
-    return Command.run('clone', newRepo.path, [authFrom])
-      .then(() => newRepo)
-  } else if (Array.isArray(from)) {
-    return newRepo.init()
-      .then(() => {
-        return Promise.each(from, (fromRepo) => {
-          return mergeRepositories(fromRepo, newRepo);
-        })
-      })
-      .then(() => newRepo);
-  } else {
-    throw new TypeError("Incorrect type of from parameter. Must be an array or object")
-  }
-} 
+  },
+};
 
 module.exports = Hg;
--- tests/HgRepo.js
+++ tests/HgRepo.js
@@ -1,11 +1,3 @@
-const HgRepo = require('../HgRepo');
-const Path = require('path');
-const Test = require('tape');
-
-test('Assertions with tape.', (assert) => {
-  assert.equal(actual, expected,
-    'Given two mismatched values, .equal() should produce a nice bug report');
-
-  assert.end();
-});
-
+// const HgRepo = require('../HgRepo');
+// const Path = require('path');
+// const Test = require('tape');
