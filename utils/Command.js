70945be85ef34883eadc3823bd09c614e771bf72
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2016-12-29 10:55:28 -0500

    Updating version of submodule

--- utils/Command.js
+++ utils/Command.js
@@ -1,10 +1,13 @@
 const Exec = require('child_process').exec;
 const Promise = require('bluebird');
 
+const Log = console;
+
 function run(command, directory = process.cwd(), options = []) {
   return new Promise((resolve, reject) => {
     const commandString = `${command} ${options.join(' ')}`;
-
+    Log.log(commandString);
+    Log.log(directory);
     Exec(commandString, { cwd: directory }, (error, stdout, stderr) => {
       const output = { error, stdout, stderr };
       if (error) {
