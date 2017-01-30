257eef2f2d6dd2ff0444508f6c726f154d7b3667
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2017-01-29 22:27:07 -0500

    Finishing up test

--- tests/Hg.js
+++ tests/Hg.js
@@ -5,6 +5,7 @@ const IsThere = require('is-there');
 const Fs = require('fs-extra-promise');
 const Command = require('../utils/Command');
 const Promise = require('bluebird');
+const DirectoryCompare = require('dir-compare');
 
 function deleteTestRepositories() {
   return Fs.removeAsync(Path.resolve('tests', 'test-repositories'))
@@ -61,16 +62,16 @@ Test('Setting the python path', (assert) => {
 Test('Cloning multiple local Hg repositories into one.', (assert) => {
   const testRepo1 = Path.resolve('tests', 'test-repositories', 'repository1');
   const testRepo2 = Path.resolve('tests', 'test-repositories', 'repository2');
-  const outputDir = Path.resolve('tests', 'results', 'Hg', 'clone-multiple', 'local');
+  const outputDirectory = Path.resolve('tests', 'results', 'Hg', 'clone-multiple', 'local');
 
-  const to = { path: outputDir };
+  const to = { path: outputDirectory };
 
   // Test that files exist
   return Hg.clone([testRepo1, testRepo2], to)
     .then(() => {
-      const subFolder1 = Path.resolve(outputDir, 'repository1');
+      const subFolder1 = Path.resolve(outputDirectory, 'repository1');
       const file1 = Path.resolve(subFolder1, 'ReadMe1.txt');
-      const subFolder2 = Path.resolve(outputDir, 'repository2');
+      const subFolder2 = Path.resolve(outputDirectory, 'repository2');
       const file2 = Path.resolve(subFolder2, 'ReadMe2.txt');
 
       assert.true(IsThere(file1), 'The file ReadMe1.txt in repository1 exists');
@@ -81,31 +82,34 @@ Test('Cloning multiple local Hg repositories into one.', (assert) => {
 Test('Cloning multiple live Hg repositories into one.', (assert) => {
   const testRepo1 = 'https://bitbucket.org/mchaput/whoosh';
   const testRepo2 = 'https://bitbucket.org/durin42/hg-git';
-  const outputDir = Path.resolve('tests', 'results', 'Hg', 'clone-multiple', 'live');
+  const outputDirectory = Path.resolve('tests', 'results', 'Hg', 'clone-multiple', 'live');
+  const testDirectory = Path.resolve('tests', 'test-repositories');
 
-  const to = { path: outputDir };
+  const exclude = { excludeFilter: '.hg' };
+  const to = { path: outputDirectory };
 
   // Test that files exist
   return Hg.clone([testRepo1, testRepo2], to)
-    .then(() => {
-      const subFolder1 = Path.resolve(outputDir, 'repository1');
-      const file1 = Path.resolve(subFolder1, 'ReadMe1.txt');
-      const subFolder2 = Path.resolve(outputDir, 'repository2');
-      const file2 = Path.resolve(subFolder2, 'ReadMe2.txt');
-
-      assert.true(IsThere(file1), 'The file ReadMe1.txt in repository1 exists');
-      assert.true(IsThere(file2), 'The file ReadMe2.txt in repository2 exists');
+    .then(() => Command.run('hg clone', testDirectory, [testRepo1]))
+    .then(() => Command.run('hg clone', testDirectory, [testRepo2]))
+    .then(() => Promise.all([
+      DirectoryCompare.compare(Path.join(testDirectory, 'whoosh'), Path.join(outputDirectory, 'whoosh'), exclude),
+      DirectoryCompare.compare(Path.join(testDirectory, 'hg-git'), Path.join(outputDirectory, 'hg-git'), exclude),
+    ]))
+    .spread((compare1, compare2) => {
+      assert.true(compare1.same);
+      assert.true(compare2.same);
     });
 });
 
 Test('Cloning multiple clashing Hg repositories into one.', (assert) => {
-  const outputDir = Path.resolve('tests', 'results', 'Hg', 'clone-multiple', 'clash');
+  const outputDirectory = Path.resolve('tests', 'results', 'Hg', 'clone-multiple', 'clash');
   const testRepo1 = Path.resolve('tests', 'test-repositories', 'repository2');
   const testRepo2 = Path.resolve('tests', 'test-repositories', 'duplicate', 'repository2');
-  const to = { path: outputDir };
+  const to = { path: outputDirectory };
 
   return Hg.clone([testRepo1, testRepo2], to)
-    .then(() => Fs.readdirAsync(outputDir))
+    .then(() => Fs.readdirAsync(outputDirectory))
     .then((directories) => {
       const noHiddenDirectories = directories.filter(directory => !directory.includes('.'));
 
@@ -113,10 +117,10 @@ Test('Cloning multiple clashing Hg repositories into one.', (assert) => {
         assert.true(directory.includes('repository2'));
 
         if (directory === 'repository2') {
-          assert.true(IsThere(Path.resolve(outputDir, directory, 'ReadMe2.txt')),
+          assert.true(IsThere(Path.resolve(outputDirectory, directory, 'ReadMe2.txt')),
             'The file ReadMe2.txt in repository2 exists');
         } else {
-          assert.true(IsThere(Path.resolve(outputDir, directory, 'ReadMe3.txt')),
+          assert.true(IsThere(Path.resolve(outputDirectory, directory, 'ReadMe3.txt')),
             'The file ReadMe3.txt in repository2 exists');
         }
       });
@@ -162,27 +166,27 @@ Test('Cloning a Hg repository.', (assert) => {
 
 Test('Creating a Hg repository with basic arguments.', (assert) => {
   const to = { path: Path.resolve('tests', 'results', 'Hg', 'create', 'basic') };
-  const outputDir = Path.resolve('tests', 'results', 'Hg', 'create', 'basic');
+  const outputDirectory = Path.resolve('tests', 'results', 'Hg', 'create', 'basic');
 
   return Hg.create(to)
     .then(() => {
-      assert.true(IsThere(outputDir), 'Repo was successfully created');
+      assert.true(IsThere(outputDirectory), 'Repo was successfully created');
     });
 });
 
 
 Test('Creating a Hg repository with default args.', (assert) => {
   const originalDir = process.cwd();
-  const outputDir = Path.resolve('tests', 'results', 'Hg', 'create', 'default');
+  const outputDirectory = Path.resolve('tests', 'results', 'Hg', 'create', 'default');
 
-  return Fs.ensureDirAsync(outputDir)
+  return Fs.ensureDirAsync(outputDirectory)
     .then(() => {
-      process.chdir(outputDir);
+      process.chdir(outputDirectory);
       return Promise.resolve();
     })
     .then(() => Hg.create())
     .then(() => {
-      assert.true(IsThere(outputDir), 'Repo was successfully created');
+      assert.true(IsThere(outputDirectory), 'Repo was successfully created');
       process.chdir(originalDir);
     });
 });
